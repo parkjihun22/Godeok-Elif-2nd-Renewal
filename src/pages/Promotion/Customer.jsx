fa4f84ae5d92b6e2e4d37e7be11dcce158eff275
parkjihun22 <eyeful31@naver.com>
2026-07-07 13:27:05 +0900

    feat : 변경

--- src/pages/Promotion/Customer.jsx
+++ src/pages/Promotion/Customer.jsx
@@ -34,7 +34,7 @@ export default function Customer() {
     setResultMessage("");
 
     try {
-      const res = await fetch("https://formspree.io/f/mojoleaz", {
+      const res = await fetch("https://formspree.io/f/xgojqpvo", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
